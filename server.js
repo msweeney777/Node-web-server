c10974a805219cbe1f2a54d3eb79ab9cde9bbee9
Matt Sweeney <msweeney777@gmail.com>
2018-12-03 20:45:31 -0500

    Second commit

--- server.js
+++ server.js
@@ -3,7 +3,26 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express!');
+  //res.send('<h1>Hello Express!</h1>');
+  res.send({
+    name: 'Matt',
+    likes: [
+      'Hagiography',
+      'Geography',
+      'Mystagogy'
+    ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
 });
 
 app.listen(3000);
