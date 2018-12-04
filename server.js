16d2623804ae60d3ab44f61eace69550103a5273
Matt Sweeney <msweeney777@gmail.com>
2018-12-04 15:59:22 -0500

    Fifth commit

--- server.js
+++ server.js
@@ -7,14 +7,10 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  //res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Matt',
-    likes: [
-      'Hagiography',
-      'Geography',
-      'Mystagogy'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
   });
 });
 
